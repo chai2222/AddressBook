906bc8556cce521fb92b2bb2d48affd53044eefb
chai2222 <chaitanayajagadale6@gmail.com>
2023-01-06 08:23:56 +0530

    Parameter

---
+++ AddressBook/Contact .cs
@@ -0,0 +1,15 @@
+﻿
+namespace AddressBook
+{
+    class Contact
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public Contact(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+    }
+}
